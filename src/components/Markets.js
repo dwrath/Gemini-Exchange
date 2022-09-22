4e895b83698290179892e543e6b0c6400c7dc371
Damian Wrather <damianwrather@Damians-Mini.home>
2022-09-21 19:14:59 -0500

    19.Select Markets

---
+++ src/components/Markets.js
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import config from "../config.json";
+
+import { loadTokens } from "../store/interactions";
+
+const Markets = () => {
+  const provider = useSelector((state) => state.provider.connection);
+  const chainId = useSelector((state) => state.provider.chainId);
+
+  const dispatch = useDispatch();
+
+  const marketHandler = async (e) => {
+    loadTokens(provider, e.target.value.split(","), dispatch);
+  };
+
+  return (
+    <div className="component exchange__markets">
+      <div className="component__header">
+        <h2>Select Market</h2>
+      </div>
+
+      {chainId && config[chainId] ? (
+        <select name="markets" id="markets" onChange={marketHandler}>
+          <option
+            value={`${config[chainId].Gemini.address},${config[chainId].mETH.address}`}
+          >
+            Gemini / mETH
+          </option>
+          <option
+            value={`${config[chainId].Gemini.address},${config[chainId].mDAI.address}`}
+          >
+            Gemini / mDAI
+          </option>
+        </select>
+      ) : (
+        <div>
+          <p>Not Deployed to Network</p>
+        </div>
+      )}
+
+      <hr />
+    </div>
+  );
+};
+
+export default Markets;
