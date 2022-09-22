b788194cb0a407ea6996a85277b31c5cf6211a52
Damian Wrather <damianwrather@Damians-Mini.home>
2022-09-22 14:22:29 -0500

    20.Deposits

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import logo from "../assets/gemini4.webp";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
@@ -15,26 +16,28 @@ function Home() {
   };
   return (
     <div>
-      <button onClick={navigateToHome}>Home</button>
-      <button onClick={navigateToExchange}>Exchange</button>;
-      <button onClick={navigateToUpload}>Upload Data</button>;
-      <main className="exchange grid">
-        <section className="exchange__section--left grid">
-          {/* Markets */}
-
-          {/* Balance */}
-
-          {/* Order */}
-        </section>
-        <section className="exchange__section--right grid">
-          {/* PriceChart */}
-
-          {/* Transactions */}
-
-          {/* Trades */}
-
-          {/* OrderBook */}
-        </section>
+      <main className="main">
+        <div className="topContainer">
+          <div>
+            <img src={logo} className="geminiLogo" alt="logo"></img>
+            <h1 className="title">Welcome to the Gemini Exchange</h1>
+          </div>
+          <p className="directions">
+            Click Upload Data to upload documents or click Exchange to go to the
+            Trading Exchange
+          </p>
+        </div>
+        <div className="btnContainer">
+          <button className="btn" onClick={navigateToHome}>
+            Home
+          </button>
+          <button className="btn" onClick={navigateToUpload}>
+            Upload Data
+          </button>
+          <button className="btn" onClick={navigateToExchange}>
+            Exchange
+          </button>
+        </div>
       </main>
       {/* Alert */}
     </div>
