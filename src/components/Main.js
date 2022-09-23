e9b6c4a20e38c7af8864b38ce3939ce193f2c4b4
Damian Wrather <damianwrather@Damians-Mini.home>
2022-09-23 18:35:49 -0500

    23. Order Book

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ import {
   loadAccount,
   loadTokens,
   loadExchange,
+  loadAllOrders,
   subscribeToEvents,
 } from "../store/interactions";
 
@@ -15,6 +16,7 @@ import Navbar from "./Navbar";
 import Markets from "./Markets";
 import Balance from "./Balance";
 import Order from "./Order";
+import OrderBook from "./OrderBook";
 
 function App() {
   const dispatch = useDispatch();
@@ -49,6 +51,8 @@ function App() {
       exchangeConfig.address,
       dispatch
     );
+    // Fetch all orders: open, filled, cancelled
+    loadAllOrders(provider, exchange, dispatch);
     //Listen to events
     subscribeToEvents(exchange, dispatch);
   };
@@ -76,7 +80,7 @@ function App() {
 
           {/* Trades */}
 
-          {/* OrderBook */}
+          <OrderBook />
         </section>
       </main>
 
